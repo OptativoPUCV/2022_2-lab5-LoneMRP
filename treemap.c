b21260979fa35b2277b4f7f940d585f09ea6597f
Replit user <>
2022-09-26 03:01:08 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -125,7 +125,26 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 
 
 Pair * upperBound(TreeMap * tree, void* key) {
-    return NULL;
+  TreeNode* aux = (TreeNode *)malloc(sizeof(TreeNode));
+  tree->current=tree->root;
+  while(tree->current!=NULL ){
+    if(is_equal(tree,tree->current->pair->key,key)){
+      return tree->current->pair;
+    }
+    else{
+      if(tree->lower_than(tree->current->pair->key,key)){
+        aux->pair = tree->current->pair;
+        tree->current = tree->current->right;
+      }
+      else{
+        aux->pair = tree->current->pair;
+        tree->current = tree->current->left;
+      }
+    }
+  }
+  printf("%d",tree->current->pair->key);
+  printf("%d".aux->pair->key);
+  return aux->pair;
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
