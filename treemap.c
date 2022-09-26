fef6244bb6573a695f265bb6c3522105069306e5
Replit user <>
2022-09-26 03:04:09 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -142,8 +142,8 @@ Pair * upperBound(TreeMap * tree, void* key) {
       }
     }
   }
-  printf("%d",tree->current->pair->&key);
-  printf("%d",aux->pair->&key);
+  printf("%d",tree->current->pair->key);
+  printf("%d",aux->pair->key);
   return aux->pair;
 }
 
